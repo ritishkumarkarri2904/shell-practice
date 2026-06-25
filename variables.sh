b24cf28aa25d496da09c5b923ff6021ee78e9d8b
ritishkumarkarri2904 <ritishkumarkarri2904>
2026-06-25 18:54:36 +0530

    second changes added

--- variables.sh
+++ variables.sh
@@ -5,6 +5,6 @@ variable2=Ramesh;
 variable3=John;
 
 
-variable1="Hello, World!"
-variable2="This is a simple shell script that prints 'Hello, World!' to the terminal."
-variable3="This is a variable that can be used to store any value."
+$variable1="Hello, World!"
+$variable2="This is a simple shell script that prints 'Hello, World!' to the terminal."
+$variable3="This is a variable that can be used to store any value."
