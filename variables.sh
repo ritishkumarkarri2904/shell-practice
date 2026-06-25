da474dd3942d5487b98369c2d3d674a420926368
ritishkumarkarri2904 <ritishkumarkarri2904>
2026-06-25 18:59:39 +0530

    second changes added

--- variables.sh
+++ variables.sh
@@ -4,6 +4,6 @@ person1=Suresh;
 person2=Ramesh;
 
 
-$person1="Hello, World!"
-$person2="This is a simple shell script that prints 'Hello, World!' to the terminal."
+echo $person1="Hello, World!"
+echo $person2="This is a simple shell script that prints 'Hello, World!' to the terminal."
 
