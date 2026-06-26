bff65519be0bdbf15bf7a09c31b5d8210a655b64
ritishkumarkarri2904 <ritishkumarkarri2904>
2026-06-26 17:04:22 +0530

    first change

--- 08-variables.sh
+++ 08-variables.sh
@@ -9,3 +9,7 @@ echo "The sum of $NUM1 and $NUM2 is: $sum"
 
 PLAYERS="Sachin, Dhoni, Kohli"
 echo "The players are: $PLAYERS"
+
+echo "The first player is: ${PLAYERS[0]}"
+echo "The second player is: ${PLAYERS[1]}"
+echo "The third player is: ${PLAYERS[2]}"
