3be9c8441f27642ad68cce91b9ba0b7a5790c20e
ritishkumarkarri2904 <ritishkumarkarri2904>
2026-07-17 17:30:01 +0530

    actions

---
+++ 03-variables.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PERSON1="Huzair"
+PERSON2="Sidu"
+PERSON3="Ritish"
+
+echo "${PERSON1} : Hi ra How are you?"
+echo "${PERSON2} : I am fine how are you"
+echo "${PERSON3} : How are you guys doing?"
+echo "${PERSON1} : I am doing good"
+echo "${PERSON2} : I am doing good"
+echo "${PERSON3} : I am doing good"
+echo "${PERSON2} : What are your plans for the weekend?"
+echo "${PERSON1} : I am planning to go hiking ${PERSON2}"
+echo "${PERSON3} : I am planning to go to the beach"
