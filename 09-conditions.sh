6941ba42a88a247134ed27537d24c72db80c6620
ritishkumarkarri2904 <ritishkumarkarri2904>
2026-08-14 16:26:03 +0530

    fruits changes

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -5,8 +5,9 @@ NUMBER=$1
 if [ $NUMBER -gt 20 ]; then
     echo "The giveen $NUMBER is greater than 20"
     
-elif [ $NUMBER -lt 20 ]; then
-    echo "the given $NUMBER is less than 20"
-
-else [ $NUMBER -eq 20 ]; then
+elif [ $NUMBER -eq 20 ]; then
     echo "the given $NUMBER is equal to 20"
+
+else [ $NUMBER -lt 20 ]; then
+    echo "the given $NUMBER is less than 20"
+fi
