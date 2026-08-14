150db443777d6fac0009f0b1e4ef6c54430800fb
ritishkumarkarri2904 <ritishkumarkarri2904>
2026-08-14 16:57:15 +0530

    fruits changes

--- 10-installaions.sh
+++ 10-installaions.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
 
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "Please run this script as root or with sudo privileges."
+    
+fi
+
+echo "Installing Nginx web server..."
 dnf install -y nginx
