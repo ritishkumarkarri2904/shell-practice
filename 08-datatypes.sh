9f657b627dbd182c4158f3f7ac72f9e08a83b3c6
ritishkumarkarri2904 <ritishkumarkarri2904>
2026-08-14 16:02:11 +0530

    fruits changes

--- 08-datatypes.sh
+++ 08-datatypes.sh
@@ -6,3 +6,13 @@ NUM2=250
 SUM=$((NUM1+NUM2))
 
 echo "The sum is: $SUM"
+
+
+#Array = list of strings or numbers. In bash, arrays are zero-indexed, meaning the first element is at index 0.
+
+
+FRUITS=("apple" "banana" "orange")
+echo "The first fruit is: ${FRUITS[0]}"
+echo "The second fruit is: ${FRUITS[1]}"
+echo "The third fruit is: ${FRUITS[2]}"
+
