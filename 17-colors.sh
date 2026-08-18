52990ba44f1d67056aaf26f16613c837cfc2b827
ritishkumarkarri2904 <ritishkumarkarri2904>
2026-08-18 18:35:40 +0530

    colors

---
+++ 17-colors.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/logs/shell-script"
+LOGS_FILE="/var/logs/shell-script/$0.log"
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[34m"
+
+if [ $USERID -ne 0 ]; then
+    echo -e " $R Please run this script as root or with sudo privileges. $N" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+#create logs folder if not exists
+mkdir -p $LOGS_FOLDER
+
+VALIDATE () {
+if [ $1 -ne 0 ]; then
+    echo -e "Installing $2 is $R failure $N" | tee -a $LOGS_FILE
+    exit 1
+else
+    echo -e "Installing $2 is $G successful $N" | tee -a $LOGS_FILE
+fi
+
+}
+
+for package in $@
+do
+    if [ $? -eq 0 ]; then
+    echo -e "$package is already installed,$Y skipping installation $N"
+else
+    echo "$package not installed, installing now"
+    dnf install -y $package
+    VALIDATE $? "$package Installation"
+fi    
+   
+done
