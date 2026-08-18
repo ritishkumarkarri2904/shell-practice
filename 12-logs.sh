0acccd65ab3bd81411316a2d01dbebc95d0903ed
ritishkumarkarri2904 <ritishkumarkarri2904>
2026-08-18 10:33:44 +0530

    changes

---
+++ 12-logs.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_Folder="/var/logs/shell-script"
+LOGS_FILE="/var/logs/shell-script/$0.log"
+
+
+if [ $USERID -ne 0 ]; then
+    echo "Please run this script as root or with sudo privileges."
+    exit 1
+fi
+
+#create logs folder if not exists
+mkdir -p $LOGS_Folder
+
+VALIDATE () {
+if [ $1 -ne 0 ]; then
+    echo "Installing $2 is failure"
+    exit 1
+else
+    echo "Installing $2 is successful"
+fi
+
+}
+
+dnf install -y nginx &>> $LOGS_FILE
+VALIDATE $? "Nginx"
+
+dnf install -y mysql-server &>> $LOGS_FILE
+VALIDATE $? "MySQL"
+
+dnf install -y nodejs &>> $LOGS_FILE
+VALIDATE $? "Nodejs"
