ed04132fe7c2e083cf8e44a920379a030bae820e
ritishkumarkarri2904 <ritishkumarkarri2904>
2026-08-18 18:28:27 +0530

    colors

--- 16-colors.sh
+++ 16-colors.sh
@@ -6,4 +6,4 @@ Y= "\e[33m"
 N= "\e[34m"
 
 
-echo -e "$R this is first color $N, $G this is second color $N, $Y this is third color  $N, $N this is the default color $N"
+echo -e "$R this is first color $N, $Y this is third color  $N"
