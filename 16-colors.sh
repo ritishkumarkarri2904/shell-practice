e4c0fb4e919db5e334b987600930d082d3798d42
ritishkumarkarri2904 <ritishkumarkarri2904>
2026-08-18 18:26:00 +0530

    colors

--- 16-colors.sh
+++ 16-colors.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-R= \e[31m
-B= \e[32m
-G= \e[33m
-Y= \e[34m
-N= \e[0m
+R= "\e[31m"
+B= "\e[32m"
+G= "\e[33m"
+Y= "\e[34m"
+N= "\e[0m"
 
 echo -e "$R this is first color $N, $B this is second color $N, $G this is third color  $N, $Y this is fourth color $N, $N this is the default color $N"
