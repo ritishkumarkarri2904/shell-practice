18c5065c7feefe64572fab9153346ce051a97a56
ritishkumarkarri2904 <ritishkumarkarri2904>
2026-08-19 14:34:02 +0530

    trap

---
+++ 19-trap.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sety -e # Exit on any error
+
+echo "My mother is best"
+echo "she will be happy to see me"
+echoo "I will be happy to see her too"
+echo "she sacrificed a lot for me"
