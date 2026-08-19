08d9048a744354a787e22ef142104eb0cc4c1977
ritishkumarkarri2904 <ritishkumarkarri2904>
2026-08-19 14:59:13 +0530

    trap

---
+++ 20-trap.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e # Exit on any error
+trap 'echo "Error: Script failed at line $LINENO, command: $BASH_COMMAND"' ERR
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/logs/shell-script"
+LOGS_FILE="/var/logs/shell-script/$0.log"
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[34m"
+
+if [ $USERID -ne 0 ]; then
+    echo -e " $R Please run this script as root or with sudo privileges. $N" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+#create logs folder if not exists
+mkdir -p $LOGS_FOLDER
+
+# we removed the validate fun bcz of set -e and trap ERR, so no need to validate the exit status of the command, if it fails it will exit the script and print the error message with line number and command.
+
+for package in $@
+do
+    if [ $? -eq 0 ]; then
+    echo -e " $G $package is already installed $N,$Y skipping installation $N"
+else
+    echo -e " $Y $package not installed, installing now $N"
+    dnf install -y $package
+fi    
+   
+done
