b50e73bd0088e7e36f9f8d971b0bf9fa850feb21
ritishkumarkarri2904 <ritishkumarkarri2904>
2026-08-19 14:41:31 +0530

    t

--- 19-trap.sh
+++ 19-trap.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-set -e # Exit on any error
+set -e #ERR # Exit on any error
+
+trap 'echo "Error: Script failed at line $LINENO, command: $BASH_COMMAND"' ERR
 
 echo "My mother is best"
 echo "she will be happy to see me"
-echoo "I will be happy to see her too"
+echo "I will be happy to see her too"
 echo "she sacrificed a lot for me"
